37d9b7d30c43bb082b47c40795af2e841cea17ff
Andrew Novokshanov <77639938+A-Novokshanov@users.noreply.github.com>
2022-01-22 12:42:52 -0500

    Organized Firebase setup

--- App.js
+++ App.js
@@ -2,6 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARCcBbAJdYul-Q6HeT5-U1qbAjOwBjwas",
+  authDomain: "cs4261-programming-1.firebaseapp.com",
+  projectId: "cs4261-programming-1",
+  storageBucket: "cs4261-programming-1.appspot.com",
+  messagingSenderId: "900136710049",
+  appId: "1:900136710049:web:c966a0a5604bd32f7265e0",
+  measurementId: "G-FFTL7MG87D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function App() {
   return (
     <View style={styles.container}>
